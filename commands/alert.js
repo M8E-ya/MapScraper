b3b3e47f78cea09dc21406915491da27f599785d
M8E-ya <suryanshu02052000@gmail.com>
2023-03-09 04:05:48 +0530

    working alerts with 2d distance calculation

--- commands/alert.js
+++ commands/alert.js
@@ -32,51 +32,59 @@ module.exports = {
 
         const town = await ccnet.getTown(townName).then(town => { return town });
 
+        let requiredNation = town.nation
+
         const residents = JSON.stringify(town.residents);
         const x = town.x;
         const z = town.z;
-        
-        setInterval(async function() {     
+
+        setInterval(async function() {
 
         const nearbyPlayers = await ccnet.getNearbyPlayers(x, z, xradius, zradius).then(players => { return players });
 
         const nonResidents = nearbyPlayers.filter(player => !residents.includes(player.name));
 
-        const playerx = nearbyPlayers.x;
-        const playerz = nearbyPlayers.z;
+        let modifiedPlayerData = nearbyPlayers.filter((item) => { return item.nation.toLowerCase() !== requiredNation.toLowerCase() })
 
-        let distance = Math.sqrt((x-playerx) ** 2 + (z-playerz) ** 2);
+        var playerDistancesdata = modifiedPlayerData.map((item) => {
+          return Math.sqrt((x - item.x) ** 2 + (z - item.z) ** 2);
+        });
+        var newPlayerData = modifiedPlayerData.map((item, index) => {
+          return { ...item, distance: playerDistancesdata[index] };
+        });
+        console.log(playerDistancesdata, newPlayerData);
 
-        console.log(nearbyPlayers)
-        console.log(playerx, playerz)
+        let distance = Math.min( ...playerDistancesdata)
 
-        let level = {}
-      if (distance <= 50) {
-        level = { color: 'FF0000', alertlevel: 'RED'}
-    } else if (distance <= 100) {
-        level = { color: 'FFA500', alertlevel: 'ORANGE'} 
-    } else if (distance <= 150) { 
-        level = {color: 'FFFF00', alertlevel: 'YELLOW'}
+        let alertcolor = ''
+        let alertlevel = ''
+      if (distance <= 75) {
+        alertcolor = 'FF0000'; alertlevel = 'RED'
+    } else if (distance <= 150) {
+        alertcolor = 'FFA500'; alertlevel = 'ORANGE'
+    } else if (distance <= 300) {
+        alertcolor = 'FFFF00'; alertlevel = 'YELLOW'
     }
 
-        if (nonResidents.length === 0) return interaction.editReply(`There are no non-residents nearby!`);
+       if (newPlayerData.length === 0) return interaction.editReply(`There are no non-residents nearby!`);
 
-    if (level.alertlevel === 'RED') {
+    if (alertlevel === 'RED') {
         alertEmbed.setTitle(`Red Alert for ${townName}!`)
-        .setDescription(`There are ${nonResidents.length} non-residents within 50 blocks! \n ${nonResidents.map(player => player.name).join(', ')}`)
-        .setColor(level.color)
-    } else if (level.alertlevel === 'ORANGE') {
+        .setDescription(`There are ${newPlayerData.length} non-residents within 75 blocks! \n ${newPlayerData.map(player => player.name).join(', ')}`)
+        .setColor(alertcolor)
+    } else if (alertlevel === 'ORANGE') {
         alertEmbed.setTitle(`Orange Alert for ${townName}!`)
-        .setDescription(`There are ${nonResidents.length} non-residents within 100 blocks! \n ${nonResidents.map(player => player.name).join(', ')}`)
-        .setColor(level.color)
-    } else if (level.alertlevel === 'YELLOW') {
+        .setDescription(`There are ${newPlayerData.length} non-residents within 150 blocks! \n ${newPlayerData.map(player => player.name).join(', ')}`)
+        .setColor(alertcolor)
+    } else if (alertlevel === 'YELLOW') {
         alertEmbed.setTitle(`Yellow Alert for ${townName}!`)
-        .setDescription(`There are ${nonResidents.length} non-residents within 150 blocks! \n ${nonResidents.map(player => player.name).join(', ')}`)
-        .setColor(level.color)
+        .setDescription(`There are ${newPlayerData.length} non-residents within 300 blocks! \n ${newPlayerData.map(player => player.name).join(', ')}`)
+        .setColor(alertcolor)
     }
 
+       console.log(alertcolor,alertlevel)
         await interaction.editReply({ embeds: [alertEmbed] });
-        //alertEmbed.setDescription(null).setColor(null).setTitle(null);
+       // alertEmbed.setDescription(null).setColor(null).setTitle(null);
     }, 15000);
 
     }
